d1224691336ad55ac9dd4394615a6cbbddd2c2d4
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2023-04-27 16:21:16 -0500

    Create `FIRST-Algorithm` - `FLC-04.27`

--- third-semester/Formal-Languages-and-Compilers/FIRST-FOLLOW-Algorithms/grammar.py
+++ third-semester/Formal-Languages-and-Compilers/FIRST-FOLLOW-Algorithms/grammar.py
@@ -1,6 +1,6 @@
 class grammar():
 
-    def __self__(self, nonterminals, terminals, productions, start):
+    def __init__(self, nonterminals, terminals, productions, start):
         self.nonterminals = nonterminals
         self.terminals = terminals
         self.productions = productions
@@ -21,9 +21,44 @@ def read_grammar():
     return nonterminals, terminals, productions, start
 
 
+def FIRST(G, symbol, P, FIRST_SET):
+    for production in P:
+        counter = 0
+        for element in production:
+            if element in G.terminals:
+                FIRST_SET[symbol].append(element)
+                break
+            if element in G.nonterminals:
+                counter += 1
+                if FIRST(G, element, G.productions[element], FIRST_SET):
+                    FIRST_SET[symbol].extend(FIRST_SET[element])
+                    FIRST_SET[symbol].remove('Ɛ')
+                else:
+                    FIRST_SET[symbol].extend(FIRST_SET[element])
+                    break
+
+                if counter == len(production):
+                    if 'Ɛ' in FIRST_SET[element]:
+                        FIRST_SET[symbol].append('Ɛ')
+
+    if 'Ɛ' in P:
+        return True
+    else:
+        return False
+                        
+
+
 def main():
     nonterminals, terminals, productions, start = read_grammar()
+    print(productions)
     G = grammar(nonterminals, terminals, productions, start)
+    FIRST_SET = {}
+    for i in nonterminals:
+        FIRST_SET[i] = []
+
+    print(FIRST_SET)
+    FIRST(G, start, productions[start], FIRST_SET)
+    print(FIRST_SET)
 
 
 if __name__ == "__main__":
